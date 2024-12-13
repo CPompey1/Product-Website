60b10afbc8afb792ff946186223cc73f4840d72e
CPompey1 <cpompey10@gmail.com>
2024-12-12 19:32:04 -0500

    more stuff

--- frontend/src/components/Pages/StorePage.jsx
+++ frontend/src/components/Pages/StorePage.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../page_components/Header'
 import LogoSection from '../page_components/LogoSection'
 import Navigation from '../page_components/Navigation'
 import SubTitle from '../page_components/global_components/stores/SubTitle'
 import { useParams } from 'react-router-dom'
 import Footer from '../page_components/Footer'
+import ProductList from '../page_components/ProductList'
 
 export default function StorePage() {
 const {storeId} = useParams();
-  return (
+const [storeData, setStoreData] = useState({})
+    useEffect(() => {
+        const fetchStoreData = async () => {
+            const response = await fetch(`/api/stores/get_store/${storeId}`, {
+                method: 'GET',
+            })
+
+            if (response.ok){
+                const jsonResult = await response.json()
+                console.log(jsonResult)
+                setStoreData(jsonResult)
+            }
+        }
+
+        fetchStoreData()
+    },[])
+
+
+    return (
     <div>
         <Header/>
         {/* Change in the future to be store specific logo */}
@@ -20,8 +39,10 @@ const {storeId} = useParams();
         <Navigation/>
         {/* Change in future to say products, store name will be given by logo */}
         <SubTitle title={storeId}/>
+        <ProductList endPoint="/api/product_list" store={storeId}/>
         <Footer/>
 
+
     </div>
-  )
+    )
 }
