21d86c64712b6ce6c4bd3e867397d85321be0ff7
CPompey1 <Cpompey10@gmail.com>
2025-09-19 21:10:44 -0400

    added springboot backend updated build script

---
+++ backend1/ebazzar/src/main/java/com/ebazzar/ebazzar/EbazzarApplication.java
@@ -0,0 +1,13 @@
+package com.ebazzar.ebazzar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbazzarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbazzarApplication.class, args);
+	}
+
+}
---
+++ backend1/ebazzar/src/test/java/com/ebazzar/ebazzar/EbazzarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebazzar.ebazzar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbazzarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
