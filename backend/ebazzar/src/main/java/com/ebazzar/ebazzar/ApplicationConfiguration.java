74c43f275461da6a7b2332de736460592b45eb5b
CPompey1 <Cpompey10@gmail.com>
2025-10-13 21:19:42 -0400

    updated cms content

--- backend/ebazzar/src/main/java/com/ebazzar/ebazzar/ApplicationConfiguration.java
+++ backend/ebazzar/src/main/java/com/ebazzar/ebazzar/ApplicationConfiguration.java
@@ -3,6 +3,8 @@ package com.ebazzar.ebazzar;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.ebazzar.ebazzar.utils.Context;
 
@@ -17,6 +19,13 @@ public class ApplicationConfiguration {
     @Bean
     public Context context() {
         return new Context();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeHttpRequests((authz) -> authz.anyRequest().permitAll());
+        return http.build();
     }
 
     
